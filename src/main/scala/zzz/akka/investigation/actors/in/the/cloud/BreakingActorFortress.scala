debec03ff740be873d0841bd42a5e4c8e4a2dc27
Alin Posorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-04-29 18:13:12 +0300

    An example of how easy it is to break the Actor Fortress.

---
+++ src/main/scala/zzz/akka/investigation/actors/in/the/cloud/BreakingActorFortress.scala
@@ -0,0 +1,49 @@
+package zzz.akka.investigation.actors.in.the.cloud
+
+import akka.actor.{ Actor, ActorRef }
+import scala.concurrent.duration._
+import akka.actor.ActorLogging
+import scala.concurrent.ExecutionContext.Implicits.global
+import akka.actor.ActorSystem
+import akka.actor.Props
+import scala.concurrent.Await
+import akka.util.Timeout
+import akka.pattern.ask// Needed for "?"
+
+case class SomeMessage(msg: String)
+case class DelayedResponse(msg: String)
+
+class DoNotDoActor extends Actor with ActorLogging {
+
+  def receive = {
+    case SomeMessage(msg) =>
+      log.info(s"Received message: $msg")
+      // This will send the DelayedResponse message to the scheduler actor instead of sending it to the sender of the 
+      // SomeMessage message.
+      context.system.scheduler.scheduleOnce(1 seconds) {
+        sender ! DelayedResponse(msg) // DO NOT DO THIS!
+      }
+    case DelayedResponse(msg) =>
+      log.info(s"We shouldn't have received the DelayedReponse message!")
+  }
+
+}
+
+object BreakingActorFortress {
+
+  def main(args: Array[String]) {
+	  val system = ActorSystem("DoNotDoActorSystem")
+	  val actor = system.actorOf(Props[DoNotDoActor])
+	
+	  actor ! SomeMessage("This is a message")
+	  
+	  implicit val timeout = Timeout(5 seconds)
+	  // This will raise an exception since no response will be returned
+//	  val response = Await.result((actor ? SomeMessage("This is my message")).mapTo[DelayedResponse], 5 seconds)
+	  
+	  system.scheduler.scheduleOnce(6 seconds){
+	    system.shutdown()
+	  }
+  }
+
+}
