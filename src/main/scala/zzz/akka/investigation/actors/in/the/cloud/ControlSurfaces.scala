95289dd6afe180d4ac39441be9ee06386f67ef55
alinposho <alinposho@yahoo,com>
2013-04-29 06:18:58 +0300

    Implement the ControlSurfaces actor.

---
+++ src/main/scala/zzz/akka/investigation/actors/in/the/cloud/ControlSurfaces.scala
@@ -0,0 +1,25 @@
+package zzz.akka.investigation.actors.in.the.cloud
+
+import akka.actor.{Actor, ActorRef}
+
+/**
+ * This object carries messages for controlling the plane.
+ */
+object ControlSurfaces {
+  case class StickBack(amount: Float)
+  case class StickForward(amount: Float)
+}
+
+// Pass in the Altimeter as an ActorRef so that we can send messages to it.
+class ControlSurfaces(altimeter: ActorRef) extends Actor {
+  import ControlSurfaces._
+  import Altimeter._
+  
+  override def receive() = {
+    case StickBack(amount) => 
+      altimeter ! RateChange(amount)
+    case StickForward(amount) => 
+      altimeter ! RateChange(-1 * amount)
+  }
+}
+
