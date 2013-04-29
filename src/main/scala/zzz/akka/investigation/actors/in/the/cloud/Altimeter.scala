518afcf5f002dab75447fb60006659118196d360
alinposho <alinposho@yahoo,com>
2013-04-29 06:18:42 +0300

    Implement the Altimeter actor.

--- src/main/scala/zzz/akka/investigation/actors/in/the/cloud/Altimeter.scala
+++ src/main/scala/zzz/akka/investigation/actors/in/the/cloud/Altimeter.scala
@@ -2,13 +2,52 @@ package zzz.akka.investigation.actors.in.the.cloud
 
 import akka.actor.{ Actor, ActorLogging, ActorSystem, Props }
 // The duration package  objects extends Ints with some timing functionality
-import scala.concurrent.duration.DurationInt
-import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration._
+// The scheduler needs and excution context - we'll just use the global one
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Altimeter {
+  // This message is sent to the Altimeter to inform about the rate of climb
+  case class RateChange(amount: Float)
+}
 
 class Altimeter extends Actor with ActorLogging {
 
+  import Altimeter.RateChange
+
+  val maxCeilingInFeet = 43000
+  // In "feet per minute"
+  val maxRateOfClimb = 5000
+  var rateOfClimb: Float = 0
+  var altitude = 0.0 // Initialized to Double
+  // As time passes, we need to change the altitude based on the time passed.
+  // The lastTick allows us to figure out how much time has passed
+  var lastTick = System.currentTimeMillis()
+
+  val ticker = context.system.scheduler.schedule(100.millis, 100.millis, self, Tick)
+  case object Tick
+
   override def receive = {
+    case RateChange(amount) =>
+      rateOfClimb = keepTheValueWithin(1.0f, -1.0f, amount) // Is this correct? It doesn't sound right! 
+      log.info(s"Altimeter changed rate of climb to $rateOfClimb.")
+
+    // Calculate a new altitude
+    case Tick =>
+      val tick = System.currentTimeMillis
+      altitude = altitude + ((tick - lastTick) / 60000.0) * rateOfClimb
+      log.info(s"Chnaged current altitude to $altitude")
+      lastTick = tick
     case msg =>
       throw new RuntimeException("Unknown message received: " + msg);
+
   }
+
+  private def keepTheValueWithin(min: Float, max: Float, amount: Float) = {
+    amount.min(min).max(max) * maxRateOfClimb
+  }
+
+  // Kill our ticker when we stop
+  override def postStop(): Unit = ticker.cancel
 }
+
