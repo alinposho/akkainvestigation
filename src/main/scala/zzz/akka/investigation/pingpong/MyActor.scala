4e4a015a3a0fe941b7ff4c94461c71463f47418a
alinposho <alinposho@yahoo,com>
2013-05-05 23:39:40 +0300

    Add an example of how not to use parallel test execution - not working.

--- src/main/scala/zzz/akka/investigation/pingpong/MyActor.scala
+++ src/main/scala/zzz/akka/investigation/pingpong/MyActor.scala
@@ -2,18 +2,23 @@ package zzz.akka.investigation.pingpong
 
 import akka.actor.Actor
 import akka.actor.ActorLogging
+import akka.actor.Props
 
 object MyActor {
   case class Ping
   case class Pong
+  
+  def apply(name: String): Props = Props(classOf[MyActor], name)  
 }
 
+class MyActor(name: String) extends Actor with ActorLogging {
 
-class MyActor extends Actor with ActorLogging{
+  assert(name == "MyActor")
+
   import MyActor._
-  
-	override def receive = {
-	  case Ping => sender ! Pong
-	  case pong => sender ! Ping
-	}
+
+  override def receive = {
+    case Ping => sender ! Pong
+    case Pong => sender ! Ping
+  }
 }
--- src/test/scala/zzz/akka/investigation/pingpong/MyActorSpec.scala
+++ src/test/scala/zzz/akka/investigation/pingpong/MyActorSpec.scala
@@ -7,13 +7,37 @@ import org.scalatest.matchers.MustMatchers
 import org.scalatest.WordSpec
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.ParallelTestExecution
+import akka.actor.ActorRef
+import akka.actor.Props
 
 class MyActorSpec extends TestKit(ActorSystem("MyActorSpec"))
   with WordSpec
   with MustMatchers
   with BeforeAndAfterAll
   with ParallelTestExecution {
+  
+  import MyActor._
 
   override def afterAll() { system.shutdown() }
+  def makeActor(): ActorRef = system.actorOf(Props(classOf[MyActor], "MyActor"))
+
+  "My Actor" should {
+    "throw an exception if it's constructed with the wrong name" in {
+      evaluating {
+        val a = system.actorOf(Props[MyActor]) // use a generated name
+      } must produce[Exception]
+    }
+
+    "construct without exception" in {
+      val a = makeActor()
+      // The throw will cause the test to fail
+    }
+
+    "respond with a Pong to a Ping" in {
+      val a = makeActor()
+      a ! Ping
+      expectMsg(Pong)
+    }
+  }
 
 }
