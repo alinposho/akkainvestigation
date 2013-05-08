75c775866484a483df07998274ca83e577b97880
alinposho <alinposho@yahoo,com>
2013-05-08 06:11:45 +0300

    Fix the call to a deprecated method.

--- src/main/scala/zzz/akka/investigation/ActorMessages.scala
+++ src/main/scala/zzz/akka/investigation/ActorMessages.scala
@@ -14,28 +14,28 @@ class MyActor extends Actor {
     case 42 =>
       println("I don't know the question. Go ask Eath Mark II.")
     case s: String =>
-      println("You sent me the string: \"" + s + "\"" )
+      println("You sent me the string: \"" + s + "\"")
     case Alpha(Beta(beta1, Gamma(gamma1)), Beta(beta2, Gamma(gamma2))) =>
       println(s"beta1: %s, beta2: %s, gamma1: %s, gamma2: %s".format(beta1, beta2, gamma1, gamma2))
-    case _ => 
+    case _ =>
       println("Huh?")
   }
 }
-
 object ActorMessages {
 
   def main(args: Array[String]) {
-	  val system = ActorSystem("MySystem")
-	  val actor = system.actorOf(Props[MyActor])
-	  
-	  actor ! "Hello"
-	  actor ! "Some String"
-	  actor ! 42
-	  actor ! Alpha(Beta("beta1", Gamma("gamma1")), Beta("beta2", Gamma("gamma2")))
-	  actor ! 456.5F
-	  actor tell """This is a message sent using the "tell" method """
-	  actor tell ("""This is a message sent using the "tell(msg, null)" method """, null)
-	  
-	  system.shutdown()
+    val system = ActorSystem("MySystem")
+    val actor = system.actorOf(Props[MyActor])
+
+    actor ! "Hello"
+    actor ! "Some String"
+    actor ! 42
+    actor ! Alpha(Beta("beta1", Gamma("gamma1")), Beta("beta2", Gamma("gamma2")))
+    actor ! 456.5F
+    actor tell ("""This is a message sent using the "tell(msg, null)" method """, null)
+    // The following call will compile and run, however, it's deprecated!
+    //actor tell """This is a message sent using the "tell" method """
+
+    system.shutdown()
   }
 }
