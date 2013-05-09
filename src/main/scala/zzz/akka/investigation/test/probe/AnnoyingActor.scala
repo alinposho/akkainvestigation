ce42ff48043bc72b2331ec81bd7b360ced1c0048
alinposho <alinposho@yahoo,com>
2013-05-09 06:56:13 +0300

    Add test example illustrating the need to test probes.

---
+++ src/main/scala/zzz/akka/investigation/test/probe/AnnoyingActor.scala
@@ -0,0 +1,25 @@
+package zzz.akka.investigation.test.probe
+
+import akka.actor.Actor
+import akka.actor.ActorRef
+
+class AnnoyingActor(snooper: ActorRef) extends Actor {
+  
+  override def preStart() = {
+    self ! 'send
+  }
+  
+  def receive = {
+    case 'send =>
+      snooper ! "Hello!!!"
+      self ! 'send
+  }
+
+}
+
+class NiceActor(snooper: ActorRef) extends Actor {
+  def receive = {
+    case 'send => 
+      snooper ! "Hi"
+  } 
+}
---
+++ src/test/scala/zzz/akka/investigation/test/probe/AnnoyingActorSpec.scala
@@ -0,0 +1,40 @@
+package zzz.akka.investigation.test.probe
+
+import akka.testkit.TestKit
+import akka.actor.ActorSystem
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.BeforeAndAfterAll
+import akka.actor.Props
+
+class AnnoyingActorSpec extends TestKit(ActorSystem("AnnoyingActor"))
+							with WordSpec 
+							with MustMatchers 
+							with BeforeAndAfterAll {
+  
+  override def afterAll() = system.shutdown()
+  
+  "The AnnoyingActor" should {
+    "say Hello!!!" in {
+      val annoying = system.actorOf(Props(classOf[AnnoyingActor], testActor))
+      expectMsg("Hello!!!")
+      system.stop(annoying)
+    }
+  }
+  
+  // This test is expected to fail from time to time since the AnnoyingActor
+  // from the previous test is not going to stop instantaneously, consequently
+  // it will continue to send "Hello!!!" messages to the testActor even during this
+  // test
+  "The NiceActor" should {
+    "say Hi!" in {
+      val nice = system.actorOf(Props(classOf[NiceActor], testActor))
+      
+      nice ! 'send
+      
+      expectMsg("Hi!")
+      system.stop(nice)
+    }
+  }
+
+}
