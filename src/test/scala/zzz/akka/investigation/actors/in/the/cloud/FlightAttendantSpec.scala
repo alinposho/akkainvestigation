2e788cc39318d380d59d57b1954885aa618d3eaf
alinposho <alinposho@yahoo,com>
2013-05-09 21:27:36 +0300

    Add a simple test for the FlightAttendant class.

---
+++ src/test/scala/zzz/akka/investigation/actors/in/the/cloud/FlightAttendantSpec.scala
@@ -0,0 +1,43 @@
+package zzz.akka.investigation.actors.in.the.cloud
+
+import akka.actor.Props
+import akka.testkit.TestKit
+import akka.actor.ActorSystem
+import org.scalatest.WordSpec
+import akka.testkit.ImplicitSender
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.BeforeAndAfterAll
+
+object TestFlightAttendant {
+  
+  class TestFlightAttendant extends FlightAttendant with AttendantResponsiveness {
+    val ONE_MILLI = 1
+    override val maxResponseTimeMS = ONE_MILLI
+  }
+  
+  def apply(): Props = Props[TestFlightAttendant]
+}
+
+class FlightAttendantSpec extends TestKit(ActorSystem("FlightAttendantTestSystem"))
+							with WordSpec
+							with ImplicitSender
+							with MustMatchers 
+							with BeforeAndAfterAll {
+  
+  import FlightAttendant._
+  
+  val Soda = "Soda"
+  
+  override def afterAll() = system.shutdown()
+  
+  "FlightAttendant" should {
+    "get a drink when asked" in {
+      val attendant = system.actorOf(TestFlightAttendant())
+      
+      attendant ! GetDrink(Soda)
+      
+      expectMsg(Drink(Soda))
+    }
+  }  
+
+}
