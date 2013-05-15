f9c2ea30f407ea593e9ac8af6f549020455d76d2
Alin Posorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-05-15 17:40:57 +0300

    Add Actor class to isolate the node that stops its children.

--- src/main/scala/zzz/akka/investigation/actors/in/the/cloud/supervisor/IsolatedStopSupervisor.scala
+++ src/main/scala/zzz/akka/investigation/actors/in/the/cloud/supervisor/IsolatedStopSupervisor.scala
@@ -2,20 +2,19 @@ package zzz.akka.investigation.actors.in.the.cloud.supervisor
 
 import scala.concurrent.duration.Duration
 import akka.actor.ActorInitializationException
-import akka.actor.SupervisorStrategy._
 import akka.actor.ActorKilledException
+import akka.actor.SupervisorStrategy._
 import SupervisionStrategyFactory._
 
-abstract class IsolatedResumeSupervisor(maxNrRetries: Int = InfinteNumberOfRetries,
-  withinTimeRange: Duration = Duration.Inf)
+abstract class IsolatedStopSupervisor(
+  maxNrRetries: Int = InfinteNumberOfRetries, withinTimeRange: Duration = Duration.Inf)
   extends IsolatedLifeCycleSupervisor {
   this: SupervisionStrategyFactory =>
-
+    
   override val supervisorStrategy = makeStrategy(maxNrRetries, withinTimeRange) {
     case _: ActorInitializationException => Stop
     case _: ActorKilledException => Stop
-    case _: Exception => Resume
+    case _: Exception => Stop
     case _ => Escalate
   }
-
 }
