988cab9be7cc3526debf71e79d522f1a151ffa27
Alin Posorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-05-15 16:42:10 +0300

    Rename some fields from SupervisionStrategy.

--- src/main/scala/zzz/akka/investigation/actors/in/the/cloud/supervisor/SupervisionStrategy.scala
+++ src/main/scala/zzz/akka/investigation/actors/in/the/cloud/supervisor/SupervisionStrategy.scala
@@ -7,20 +7,25 @@ import akka.actor.OneForOneStrategy
 import akka.actor.AllForOneStrategy
 import akka.actor.AllForOneStrategy
 
+object SupervisionStrategyFactory {
+  val InfinteNumberOfRetries = -1;
+}
+
 trait SupervisionStrategyFactory {
-  def makeStrategy(maxNrRetries: Int, withinRimeRange: Duration)(decider: Decider): SupervisorStrategy
+  def makeStrategy(maxNrRetries: Int, withinTimeRange: Duration)(decider: Decider): SupervisorStrategy
 }
 
 // Replace the following two traits with just one, providing the class to be instantiated as a parameter in the 
 // constructor
 trait OneForOneSupervisionStrategy extends SupervisionStrategyFactory {
-  def makeStrategy(maxNrRetries: Int, withinRimeRange: Duration)(decider: Decider) = {
-    OneForOneStrategy(maxNrRetries, withinRimeRange)(decider)
+  def makeStrategy(maxNrRetries: Int, withinTimeRange: Duration)(decider: Decider) = {
+    OneForOneStrategy(maxNrRetries, withinTimeRange)(decider)
   }
 }
 
 trait AllForOneSupervisionStrategy extends SupervisionStrategyFactory {
-  def makeStrategy(maxNrRetries: Int, withinRimeRange: Duration)(decider: Decider) = {
-    AllForOneStrategy(maxNrRetries, withinRimeRange)(decider)
+  def makeStrategy(maxNrRetries: Int, withinTimeRange: Duration)(decider: Decider) = {
+    AllForOneStrategy(maxNrRetries, withinTimeRange)(decider)
   }
 }
+
