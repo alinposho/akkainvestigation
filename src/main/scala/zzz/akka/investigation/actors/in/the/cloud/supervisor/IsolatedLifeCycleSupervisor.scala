148ce3eb99b1517cb1fcfee34e254d3a5b551be5
alinposho <alinposho@yahoo,com>
2013-05-15 06:42:21 +0300

    Define the IsolatedLifeCycleSupervisor trait.

---
+++ src/main/scala/zzz/akka/investigation/actors/in/the/cloud/supervisor/IsolatedLifeCycleSupervisor.scala
@@ -0,0 +1,31 @@
+package zzz.akka.investigation.actors.in.the.cloud.supervisor
+
+import akka.actor.Actor
+
+/**
+ * Defines messages exchanged with actors waiting for this actor to finish
+ * starting.
+ */
+object IsolatedLifeCycleSupervisor {
+  case object WaitForStart
+  case object Started
+}
+
+trait IsolatedLifeCycleSupervisor extends Actor {
+  import IsolatedLifeCycleSupervisor._
+
+  def receive = {
+    case WaitForStart =>
+      sender ! Started
+    case msg =>
+      throw new Exception(s"Don't call ${self.path.name} directly (${msg}).")
+  }
+
+  def childStarter(): Unit
+
+  final override def preStart() { childStarter() }
+
+  final override def preRestart(reason: Throwable, message: Option[Any]) {}
+  final override def postRestart(reason: Throwable) {}
+  
+}
