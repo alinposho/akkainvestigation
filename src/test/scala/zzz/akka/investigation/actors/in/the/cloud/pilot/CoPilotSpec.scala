33171c79c074da5795ade37a7e85230d216d0b1f
Alin Posorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-06-20 17:30:51 +0300

    Refactor CoPilotSpec test class for clarity.

--- src/test/scala/zzz/akka/investigation/actors/in/the/cloud/pilot/CoPilotSpec.scala
+++ src/test/scala/zzz/akka/investigation/actors/in/the/cloud/pilot/CoPilotSpec.scala
@@ -18,6 +18,54 @@ import akka.pattern.ask
 import zzz.akka.investigation.actors.in.the.cloud.Plane._
 import com.typesafe.config.ConfigFactory
 
+@RunWith(classOf[JUnitRunner])
+class CoPilotSpec extends TestKit(ActorSystem("CopilotSpec", ConfigFactory.parseString(CoPilotSpec.configStr))) 
+					with MustMatchers
+					with ImplicitSender
+					with WordSpec 
+					with BeforeAndAfterAll {
+
+  val pilotPath = s"/user/TestPilots/$pilotName"
+  val copilotPath = s"/user/TestPilots/$copilotName"
+  val TestPilotsSupervisorName = "TestPilots"
+  
+  override def afterAll = system.shutdown()
+  
+  "Copilot" should {
+    "take control of the plane when the Pilot dies" in {
+      pilotsReadyToGo()
+      
+      killPilot()
+      
+      makeSureCopilotTookControlOfThePlane()
+    }
+  }
+
+  def pilotsReadyToGo(): ActorRef = {
+    val supervisor = system.actorOf(Props(classOf[PilotsSupervisor], testActor), TestPilotsSupervisorName)
+    waitForSupervisorAndChildrenSetUp(supervisor)
+    system.actorFor(copilotPath) ! Pilots.ReadyToGo
+
+    supervisor
+  }
+
+  private def waitForSupervisorAndChildrenSetUp(supervisor: akka.actor.ActorRef): Any = {
+    implicit val askTimeout = Timeout(4 seconds)
+    Await.result(supervisor ? IsolatedLifeCycleSupervisor.WaitForStart, 3 seconds)
+  }
+    
+
+  def killPilot() {
+    system.actorFor(pilotPath) ! "throw"
+  }
+
+  def makeSureCopilotTookControlOfThePlane() {
+    expectMsg(GiveMeControl)
+    lastSender must be(system.actorFor(copilotPath))
+  }
+
+}
+
 class FakePilot extends Actor {
   override def receive = {
     case _ => throw new Exception("This exception is expected.")
@@ -49,48 +97,4 @@ class PilotsSupervisor(testActor: ActorRef)
     context.actorOf(Props[FakePilot], pilotName)
     context.actorOf(Props(classOf[CoPilot], testActor, nilActor, nilActor), copilotName)
   }
-}
-
-@RunWith(classOf[JUnitRunner])
-class CoPilotSpec extends TestKit(ActorSystem("CopilotSpec", ConfigFactory.parseString(CoPilotSpec.configStr))) 
-					with MustMatchers
-					with ImplicitSender
-					with WordSpec 
-					with BeforeAndAfterAll {
-
-  val pilotPath = s"/user/TestPilots/$pilotName"
-  val copilotPath = s"/user/TestPilots/$copilotName"
-  
-  override def afterAll = system.shutdown()
-  
-  def pilotsReadyToGo(): ActorRef = {
-	implicit val askTimeout = Timeout(4 seconds)
-	val supervisor = system.actorOf(Props(classOf[PilotsSupervisor], testActor), "TestPilots")
-	Await.result(supervisor ? IsolatedLifeCycleSupervisor.WaitForStart, 3 seconds)
-	
-	system.actorFor(copilotPath) ! Pilots.ReadyToGo
-
-	supervisor
-  }
-    
-  "Copilot" should {
-    "take control of the plane when the Pilot dies" in {
-      pilotsReadyToGo()
-      
-      killPilot()
-      
-      makeSureCopilotTookControlOfThePlane()
-    }
-  }
-
-  def killPilot() {
-    system.actorFor(pilotPath) ! "throw"
-  }
-
-  def makeSureCopilotTookControlOfThePlane() {
-    expectMsg(GiveMeControl)
-    // The girl who sent it had better be Mary
-    lastSender must be(system.actorFor(copilotPath))
-  }
-
 }
