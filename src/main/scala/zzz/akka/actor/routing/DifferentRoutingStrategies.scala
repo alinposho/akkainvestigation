00c51dd1c4cda841eaae829b0158fd3eb46ca7ce
alinposho <alinposho@yahoo.com>
2013-10-23 07:28:12 +0100

    Attempt to illustrate different Actor routing strategies

--- src/main/scala/zzz/akka/actor/routing/DifferentRoutingStrategies.scala
+++ src/main/scala/zzz/akka/actor/routing/DifferentRoutingStrategies.scala
@@ -1,7 +1,6 @@
 package zzz.akka.actor.routing
 
 import com.typesafe.config.ConfigFactory
-
 import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.actor.ActorSystem
@@ -9,23 +8,36 @@ import akka.actor.Props
 import akka.actor.actorRef2Scala
 import akka.routing.FromConfig
 import scala.concurrent.duration._
+import akka.actor.ActorRef
+import scala.util.Random
 
-object RoundRobinRoutingTest {
+object DifferentRoutingStrategies {
 
   def main(args: Array[String]) {
-    val system = ActorSystem("RoundRobinActosSystemTest")
+    val system = ActorSystem("RoutingStrategisSystem")
     // In order for this to work, the configuration file "application.conf" has to be in the classpath and
     // it has to contain the configuration for the "DatabaseConnectionRouter" under "akka { actor { configuration {...}}}"
     // otherwise it won't work. On further inspection one might find it possible to customize that.
-    val dbRouter = system.actorOf(Props[DBConnection].withRouter(FromConfig()), "DatabaseConnectionRouter")
-
-    for (no <- 1 to 40) {
-      dbRouter ! "GetMeNewConnection"
-    }
-
+    val roundRobinRouter = system.actorOf(Props[DBConnection].withRouter(FromConfig()), "RoundRobinRouter")
+    val smallestMailboxRouter = system.actorOf(Props[DBConnection].withRouter(FromConfig()), "SmallestMaiboxRouter")
+    
+    println("Round robin routing strategy")
+    sendRequestsTo(roundRobinRouter)
+    
+    Thread.sleep(2000)
+    
+    println("\n\nSmallest mailbox routing strategy")
+    sendRequestsTo(smallestMailboxRouter)
+    
     import scala.concurrent.ExecutionContext.Implicits.global
     system.scheduler.scheduleOnce(5 seconds) {
       system.shutdown()
+    }
+  }
+
+  private def sendRequestsTo(router: ActorRef): Unit = {
+    for (no <- 1 to 40) yield {
+      router ! "GetMeNewConnection" + no
     }
   }
 
@@ -33,6 +45,8 @@ object RoundRobinRoutingTest {
 
 class DBConnection extends Actor with ActorLogging {
   override def receive = {
-    case _ => log.info(s"${self} received a new DB connection request")
+    case connection => 
+      Thread.sleep((100 * Math.random()).toInt)
+      log.info(s"${self} received a new DB connection request ${connection}")
   }
 }
